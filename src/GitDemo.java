7a13d428dc5f3c8096a985899deae933ec1c0e97
zzp1230 <z460890525@163.com>
2016-10-31 11:02:03 +0800

    update

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
         System.out.println("git on idea");
         System.out.println("what is your name?");
         System.out.println("change back to master");
+        System.out.println("");
     }
 }
